5698bbdf274a68ebb9c8a317ff046e0bda27e2c4
Amine_88 <amin.ouni@neopolis-dev.com>
2024-10-13 10:34:32 +0100

    chore: add error handler signup model

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['user', 'guide', 'lead-guide', 'admin'],
+        enum: ['user', 'project-manager', 'admin'],
         default: 'user'
     },
     password: {
